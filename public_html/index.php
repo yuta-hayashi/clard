9e0a9ebac11620d135b38b900a14f3f117fd4871
yt-hayashi <h.yuta999@gmail.com>
2017-10-07 23:32:27 +0900

    デプロイテスト２

--- public_html/index.php
+++ public_html/index.php
@@ -54,7 +54,7 @@ $stmt = $pdo->query($sql);
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/1.11.8/semantic.min.css"/>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/1.11.8/semantic.min.js"></script>
-
+<!--デプロイテスト-->
 
 
 </head>
